235a10f789784b05d713d1c424c48ae8b4f7b069
Gustavo <gustavo7dias@gmail.com>
2020-11-19 21:58:07 -0300

    Setting main button and funcs

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,32 @@
 function printAdvice(data) {
     adviceResponse.innerText = data.slip.advice;
+};
+
+function randomImage() {
+    let randomNum = Math.floor(Math.random() * 5) + 1;
+    imageResponse.src = `/assets/goat-0${randomNum}.jpg`;
+};
+
+function randomAdvice() {
+    let endpoint = 'https://api.adviceslip.com/advice';
+    fetch(endpoint) //send our request which returns a promise
+        .then((response => { // .then() = to have access to the response that the API has provided, and then we returned it as JSON.
+            return response.json();
+        }))
+        .then((data => {
+            printAdvice(data);
+        }));
 }
 
-
-let endpoint = 'https://api.adviceslip.com/advice';
-fetch(endpoint) //send our request which returns a promise
-    .then((response => { // .then() = to have access to the response that the API has provided, and then we returned it as JSON.
-        return response.json();
-    }))
-    .then((data => {
-        printAdvice(data);
-    }));
-
+function randomAll() {
+    randomAdvice();
+    randomImage();
+}
 
 const adviceResponse = document.getElementById('response-advice');
 const imageResponse = document.getElementById('response-image');
+const mainButton = document.getElementById('main-button');
 
-let randomNum = Math.floor(Math.random() * 5) + 1;
-console.log(randomNum);
-imageResponse.src = `/assets/goat-0${randomNum}.jpg`;
+randomAdvice();
+randomImage();
+mainButton = addEventListener('click', randomAll);

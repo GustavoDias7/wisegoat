599d5aaf6a8c5f7b59b9518dab921566a24d4e81
Gustavo <gustavo7dias@gmail.com>
2020-12-31 12:54:54 -0300

    Update structure and fetch

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,18 @@
-function printAdvice(data) {
-    adviceResponse.innerText = data.slip.advice;
-};
+let adviceResponse = document.getElementById('response-advice');
+let mainButton = document.getElementById('main-button');
+mainButton.addEventListener('click', randomAdvice);
 
 function randomAdvice() {
     let endpoint = 'https://api.adviceslip.com/advice';
     fetch(endpoint) //send our request which returns a promise
-        .then((response => { // .then() = to have access to the response that the API has provided, and then we returned it as JSON.
-            return response.json();
-        }))
-        .then((data => {
-            printAdvice(data);
-        }));
+    // .then() = to have access to the response that the API has provided, and then we RETURN it as JSON.
+        .then(response => response.json())
+        .then(printAdvice)
+        .catch(console.error);
+};
+
+function printAdvice(data) {
+    adviceResponse.innerText = data.slip.advice;
 };
 
 randomAdvice();
-
-let adviceResponse = document.getElementById('response-advice');
-let mainButton = document.getElementById('main-button');
-mainButton.addEventListener('click', randomAdvice);
